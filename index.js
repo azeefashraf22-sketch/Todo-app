93cae7c7d8c7b68dc7f068c4841ccc265738e58a
azeefashraf22sketch <azeefashraf22@gmail.com>
2026-08-03 11:24:30 -0700

    Refactor server setup and update dependencies in package files

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-import express from 'express'
-import dotenv from 'dotenv'
-dotenv.config()
+import express from 'express';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import path from 'path';
+import bodyParser from 'body-parser';
+dotenv.config();
 
+const app = express();
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(process.cwd())));
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
