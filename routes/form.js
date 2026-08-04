077cabed08dfb7708ad2b37c4498d049eeccaefd
azeefashraf22sketch <azeefashraf22@gmail.com>
2026-08-04 04:22:27 -0700

    Fix form submission route and update form view

--- routes/form.js
+++ routes/form.js
@@ -1,6 +1,10 @@
 import express from 'express';
 const router = express.Router();
 
+router.get("/", (req,res) => {
+    res.render("form")
+})
+
 router.post("/submit", (req, res) => {
     res.send(req.body);
 });
