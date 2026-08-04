c0df1226f14e323920ff21220ae0d8cadae195c2
azeefashraf22sketch <azeefashraf22@gmail.com>
2026-08-04 11:44:15 -0700

    Update form and todo routes for improved functionality and user experience

--- routes/form.js
+++ routes/form.js
@@ -1,9 +1,9 @@
 import express from 'express';
 const router = express.Router();
 
-router.get("/", (req,res) => {
-    res.render("form")
-})
+router.get("/", (req, res) => {
+    res.render("form", { user: req.query.user || 'User' });
+});
 
 router.post("/submit", (req, res) => {
     res.send(req.body);
--- routes/todo.js
+++ routes/todo.js
@@ -3,12 +3,12 @@ import { addTodo, getTodos } from '../controllers/todo.js';
 const router = express.Router();
 
 router.post("/add", (req, res) => {
-    addTodo(req.body.data)
-    res.send("todo added!");
+    addTodo(req.body.data);
+    res.redirect("/todo");
 });
 
-router.get("/", (req,res) => {
-    res.render("todo",{todos:getTodos()});
-})
+router.get("/", (req, res) => {
+    res.render("todo", { todos: getTodos() });
+});
 
 export default router;
